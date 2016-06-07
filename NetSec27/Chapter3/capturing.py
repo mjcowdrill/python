885858eb0025e8f0c1b3c7c1bc7341defe6e0215
MJC <mark.cowdrill@googlemail.com>
2016-06-07 10:27:21 +0100

    Add ip addresses and name lookups

--- NetSec27/Chapter3/capturing.py
+++ NetSec27/Chapter3/capturing.py
@@ -4,21 +4,38 @@ import pcapy
 import sys
 from pprint import pprint as pp
 from struct import pack, unpack
+from time import sleep
+from socket import gethostbyaddr
 
 devs = pcapy.findalldevs()
 print(devs)
 
+if "wlan0" in devs:
+    adapter="wlan0"
+else:
+    adapter="eth0"
+
+print("adapter=", adapter)
+
 #  device, # of byte to capture per packet, promiscuous mode, timeout (ms)
 #cap = pcapy.open_live("eth0", 65536 , 1 , 0)
-cap = pcapy.open_live("wlan0", 65536 , 1 , 0)
+cap = pcapy.open_live(adapter, 65536 , 1 , 0)
+sleep(1)
 
 #z = ("cc", "3d", "82", "08", "8e", "dc") 
-z = []
+macs = []
+ips  = []
 
 count = 1
 while True:
     #sys.stdout.write(".")
-    (header, payload) = cap.next()
+
+    try:
+        (header, payload) = cap.next()
+    except:
+        (header, payload) = (None, None)
+        sleep(1)
+
     if payload and len(payload) > 0:
         ipheader   = unpack("!BBHHHBBH4s4s", payload[14:34])
         timetolive = ipheader[5]
@@ -27,13 +44,22 @@ while True:
         a1 = payload[:6]
         a2 = payload[6:12]
         y  = ".".join([ "%.2x" % ord(x) for x in a1 ])
-        if y not in z:
-            z.append(y)
+        if y not in macs:
+            macs.append(y)
             print y, timetolive, protocol
         y  = ".".join([ "%.2x" % ord(x) for x in a1 ])
-        if y not in z:
-            z.append(y)
+        if y not in macs:
+            macs.append(y)
             print y, timetolive, protocol
+        y  = ".".join([ "%d" % ord(x) for x in ipheader[8] ])
+        if y not in ips:
+            host = ""
+            try:
+                host = gethostbyaddr(y)
+            except:
+                host = "unknown"
+            ips.append(y)
+            print y, host
 #       if z == x:
 #           print "YAY1"
 #           exit(0)
